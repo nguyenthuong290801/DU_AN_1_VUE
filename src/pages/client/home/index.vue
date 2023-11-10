f395537341317bfab22087d0129638ee1e5b71aa
HoangPhuc2703 <114477861+HoangPhuc2703@users.noreply.github.com>
2023-11-10 23:52:15 +0700

    [VUE] - UPDATE ASSETS - NGUYEN HOANG PHUC (DW04)

--- src/pages/client/home/index.vue
+++ src/pages/client/home/index.vue
@@ -1,13 +1,15 @@
 <template>
   <Banner></Banner>
+  <Category></Category>
 </template>
 
 <script>
 import Banner from '../../../components/Banner.vue';
-
+import Category from '../../../components/Category.vue'
 export default {
   components: {
     Banner,
+    Category
   }
 }
 </script>
