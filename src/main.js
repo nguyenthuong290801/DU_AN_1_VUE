4c820213f6702416bc62bef68789c334734aef09
thuongna <contact.thuongna@gmail.com>
2023-11-11 14:02:04 +0700

    [VUE] - CREATE DASHBOARD SELLER - NGUYEN ANH THUONG (DW_06)

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-
-// import VueNativeSock from "vue-native-websocket"
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-utilities.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
-
 import VueSplide from '@splidejs/vue-splide';
 import '@splidejs/vue-splide/css';
 
 const app = createApp(App)
 app.use(router)
-// app.use(VueNativeSock, "ws://your-websocket-server-url")
-// app.use(IconsPlugin)
 app.mount('#app')
 app.use( VueSplide );
 
